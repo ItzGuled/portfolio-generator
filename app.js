abde88b1794ab851014b084f2595af71726d4c1b
itzguled <itzguled55337@gmail.com>
2022-02-19 17:36:14 -0600

    add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { writeFile, copyFile} = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -130,13 +130,13 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
     return generatePage(portfolioData);
-   })
-   .then(pageHTML => {
-     return writeFile(pageHTML);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
   })
   .then(writeFileResponse => {
-  console.log(writeFileResponse);
-  return copyFile();
+    console.log(writeFileResponse);
+    return copyFile();
   })
   .then(copyFileResponse => {
     console.log(copyFileResponse);
